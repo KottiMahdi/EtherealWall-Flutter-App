48759036a5d1bb57ebaeca832f2b2154e3531d2c
Mahdi Kotti <kottimahdi22@gmail.com>
2026-04-02 12:04:24 +0100

    fix flutter CI

---
+++ test/placeholder_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('CI Placeholder Test', () {
+    // This test ensures that the CI test runner finds at least one valid test file.
+    expect(true, isTrue);
+  });
+}
